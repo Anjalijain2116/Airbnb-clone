1e2effa03b340dc5d0e859ccce1c1414400f6232
Anjalijain2116 <anjalijain6010@gmail.com>
2022-07-10 14:52:00 +0530

    Updates

---
+++ precache-manifest.988b2c536e7207a201794ae4545662dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26975842dd14dda5403f",
+    "url": "/airbnb-clone/static/css/main.79614362.chunk.css"
+  },
+  {
+    "revision": "26975842dd14dda5403f",
+    "url": "/airbnb-clone/static/js/main.26975842.chunk.js"
+  },
+  {
+    "revision": "196d49733e7cfa6e5f29",
+    "url": "/airbnb-clone/static/css/1.36153a6a.chunk.css"
+  },
+  {
+    "revision": "196d49733e7cfa6e5f29",
+    "url": "/airbnb-clone/static/js/1.196d4973.chunk.js"
+  },
+  {
+    "revision": "9675a35169f9c1d15a74",
+    "url": "/airbnb-clone/static/js/runtime~main.9675a351.js"
+  },
+  {
+    "revision": "97150f146c3446bc56d1c494ed0db5b1",
+    "url": "/airbnb-clone/index.html"
+  }
+];
